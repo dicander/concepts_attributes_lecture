6dfc2af08783fdab6aa86c10c55da18f08e7ac8e
Marcus Dicander <dicander@kth.se>
2026-04-12 23:28:41 +0200

    Added error messages

--- src/lessthan/lessthan.cpp
+++ src/lessthan/lessthan.cpp
@@ -36,7 +36,45 @@ int main() {
     //int result = 1;
     LessThanComparable auto result = a < b;
     //LessThanComparable2 auto result2 = a < b;
-    //LessThanComparable3 auto result3 = a < b;
+/*     dicander@Whirlwind:~/Documents/cprog/concepts_attributes_lecture/src/lessthan$ g++ -std=c++20 lessthan.cpp -fconcepts-diagnostics-depth=3 && ./a.out
+lessthan.cpp: In function ‘int main()’:
+lessthan.cpp:38:42: error: deduced initializer does not satisfy placeholder constraints
+   38 |     LessThanComparable2 auto result2 = a < b;
+      |                                        ~~^~~
+lessthan.cpp:38:42: note: constraints not satisfied
+lessthan.cpp:17:9:   required for the satisfaction of ‘LessThanComparable2<auto [requires ::LessThanComparable2<<placeholder>, >]>’ [with auto [requires ::LessThanComparable2<<placeholder>, >] = bool]
+lessthan.cpp:17:31:   in requirements with ‘T a’, ‘T b’ [with T = bool]
+lessthan.cpp:18:16: note: the required expression ‘operator<(a, b)’ is invalid, because
+   18 |     { operator<(a,b) } -> std::same_as<bool>; // This does not express the same thing as the previous one
+      |       ~~~~~~~~~^~~~~
+lessthan.cpp:18:16: error: ‘operator<’ not defined */
+    
+//    LessThanComparable3 auto result3 = a < b;
+/*     dicander@Whirlwind:~/Documents/cprog/concepts_attributes_lecture/src/lessthan$ g++ -std=c++20 lessthan.cpp -fconcepts-diagnostics-depth=3 && ./a.out
+lessthan.cpp: In function ‘int main()’:
+lessthan.cpp:38:42: error: deduced initializer does not satisfy placeholder constraints
+   38 |     LessThanComparable2 auto result2 = a < b;
+      |                                        ~~^~~
+lessthan.cpp:38:42: note: constraints not satisfied
+lessthan.cpp:17:9:   required for the satisfaction of ‘LessThanComparable2<auto [requires ::LessThanComparable2<<placeholder>, >]>’ [with auto [requires ::LessThanComparable2<<placeholder>, >] = bool]
+lessthan.cpp:17:31:   in requirements with ‘T a’, ‘T b’ [with T = bool]
+lessthan.cpp:18:16: note: the required expression ‘operator<(a, b)’ is invalid, because
+   18 |     { operator<(a,b) } -> std::same_as<bool>; // This does not express the same thing as the previous one
+      |       ~~~~~~~~~^~~~~
+lessthan.cpp:18:16: error: ‘operator<’ not defined
+dicander@Whirlwind:~/Documents/cprog/concepts_attributes_lecture/src/lessthan$ g++ -std=c++20 lessthan.cpp -fconcepts-diagnostics-depth=3 && ./a.out
+lessthan.cpp: In function ‘int main()’:
+lessthan.cpp:52:42: error: deduced initializer does not satisfy placeholder constraints
+   52 |     LessThanComparable3 auto result3 = a < b;
+      |                                        ~~^~~
+lessthan.cpp:52:42: note: constraints not satisfied
+lessthan.cpp:27:9:   required for the satisfaction of ‘LessThanComparable3<auto [requires ::LessThanComparable3<<placeholder>, >]>’ [with auto [requires ::LessThanComparable3<<placeholder>, >] = bool]
+lessthan.cpp:27:31:   in requirements with ‘T a’, ‘T b’ [with T = bool]
+lessthan.cpp:29:16: note: the required expression ‘operator<(a, b)’ is invalid, because
+   29 |     { operator<(a,b) } -> std::same_as<bool>;
+      |       ~~~~~~~~~^~~~~
+lessthan.cpp:29:16: error: ‘operator<’ not defined
+dicander@Whirlwind:~/Documents/cprog/concepts_attributes_lecture/src/lessthan$ */
     std::cout << result << std::endl;   
 
 }
